3264957e6c0cd869c19729aebf5a86492ae526eb
Jamie Bloomfield <jamie.bloomfield@student.manchester.ac.uk>
2022-02-21 11:11:38 +0000

    Set to use mysql db

--- src/hustlio/settings.py
+++ src/hustlio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hustlio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mytestdb',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
