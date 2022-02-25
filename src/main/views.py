f46b25480b9fa4a1530c59203bff38e00944eabb
Torin Felton <torinbrody1@gmail.com>
2022-02-25 14:37:05 +0000

    add extra view funcs

--- src/main/views.py
+++ src/main/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserSignupForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+def signup_view(request):
+	if request.method == 'POST':
+		form = forms.UserSignupForm(request.POST)
+		if form.is_valid():
+			user = form.save() # Create account
+			login(request, user)
+			return redirect('')
+	return render(request, 'accounts/signup.html')
+
+
+def login_view(request):
+	if request.method == 'POST':
+		form = forms.UserLoginForm(data=request.POST)
+		if form.is_valid():
+			# login
+			user = form.get_user()
+			login(request, user)
+			toRedirect = '/firehome/'
+			if 'next' in request.POST:
+				toRedirect = request.POST.get('next')
+			return redirect(toRedirect)
+	else:
+		form = forms.UserLoginForm()
+	return render(request, "accounts/login.html", {'form': form})
+
+def logout_view(request):
+	if request.method == 'POST':
+		logout(request)
+		return redirect('/firehome/')
