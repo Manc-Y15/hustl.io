48c30c3fe021a623db0d34dc14ae309328901999
unknown <alexander.hanley@student.manchester.ac.uk>
2022-03-02 21:42:02 +0000

    added holdings updating after transactions

--- src/main/transactions.py
+++ src/main/transactions.py
@@ -10,8 +10,13 @@ from decimal import Decimal
 # add to transaction table portfolio,stock,price,amount,datetime,isbuy
 
 def user_buy(user, is_buy, stock_id, amount):
+    # check to see if user has bought the stock before
+    stock = Stock.objects.filter(ticket = stock_id)[0]
+    if Holding.objects.filter(owner = user, stock_id = stock).exists():
+        firstBuy = False
+    else:
+        firstBuy = True
     if amount != 0:
-        stock = Stock.objects.filter(ticket = stock_id)[0]
         volume = amount / float(stock.current_price)
         if is_buy:
             purchase = 'BUY'
@@ -22,8 +27,20 @@ def user_buy(user, is_buy, stock_id, amount):
                 return False
             purchase = 'SELL'
             amount = amount * -1 # makes the amount negative. The amount is subtracted from the balance.
+            volumeSell = volume * -1
         user.portfolio.balance -= Decimal.from_float(float(amount))
-        
+        if firstBuy == False:
+            thisHolding = Holding.objects.filter(owner = user, stock_id = stock)[0]
+            if is_buy:
+                thisHolding.amount = round((thisHolding.amount + volume),2)
+            else:
+                thisHolding.amount = round((thisHolding.amount + volumeSell),2)
+        else:
+            newHolding = Holding()
+            newHolding.owner = user
+            newHolding.amount = volume
+            newHolding.stock_id = stock
+            newHolding.save()
         timeOfBuy = timezone.now()
         transac = Transaction()
         transac.portfolio_id = (user.portfolio)
