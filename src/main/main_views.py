3d58103b3f21ebc8d19402f4cdfcb899dcbb1950
unknown <alexander.hanley@student.manchester.ac.uk>
2022-03-15 12:21:28 +0000

    can add friends via backend function

--- src/main/main_views.py
+++ src/main/main_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
-from .models import Stock,Profile,Portfolio,Holding
+from .models import Stock,Profile,Portfolio,Holding,User
 
 from .holdings import get_holdings, holdings_distribution, holdings_total
 
@@ -60,7 +60,28 @@ def settings_view(request):
 	errors = []
 
 	return render(request, 'accounts/account_settings.html', {})
-
+def add_friend(request,friend_name):
+	if User.objects.filter(username = friend_name).exists():
+		newFriend =  User.objects.filter(username = friend_name)[0]
+		if newFriend not in request.user.profile.friends.all():
+			request.user.profile.friends.add(newFriend)
+			request.user.profile.save()
+			return(True,"")
+		else:
+			return(False,f"You're already friends with {friend_name}")
+	else:
+		return(False,"This user does not exist")
+def remove_friend(request,friend_name):
+	if User.objects.filter(username = friend_name).exists():
+		oldFriend =  User.objects.filter(username = friend_name)[0]
+		if oldFriend not in request.user.profile.friends.all():
+			request.user.profile.friends.remove(oldFriend)
+			request.user.profile.save()
+			return(True,"")
+		else:
+			return(False,f"You're not friends with {friend_name}")
+	else:
+		return(False,"This user does not exist")	
 def friends_view(request):
 	#getUserProfile = Profile.objects.filter(user = request.user)[0]
 	friendNames = []
@@ -71,12 +92,13 @@ def friends_view(request):
 		friendNames.append(friend.username)
 		friendsWorth.append(1000)
 
-	friends = [[friendNames[0],100.78,"21-02-2022","Losing",73.58],[friendNames[1],230.78,"10-03-2022","Winning",62.58],["Someone7321",10058.32,"03-03-2022","Winning",3256.58]]
+	friends = [[friendNames[0],100.78,"21-02-2022","Losing",73.58],[friendNames[0],230.78,"10-03-2022","Winning",62.58],["Someone7321",10058.32,"03-03-2022","Winning",3256.58]]
 
 	leagues = [["Y15 League",["Someone1191","Up"],["Someone1391","Down"],["Someone1237","Same"]],["Y20 League",["Someone2054","Same"],["Someone2978","Up"],["Someone2453","Down"]]]
-	
+	remove_friend(request,'torin')
 	return render(request, 'accounts/friends.html', {
 		'friends':friends,
 		'leagues':leagues
 		})
+	
 
