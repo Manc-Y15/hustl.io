ff769c2f21129c6c112d1c314c12866108c578b5
Torin Felton <torinbrody1@gmail.com>
2022-03-30 17:15:50 +0100

    more fixes

--- src/main/main_views.py
+++ src/main/main_views.py
@@ -36,6 +36,10 @@ def home_view(request):
 		cols = stock.display_colour.split(' ')
 		stock.col1 = cols[0]
 		stock.col2 = cols[1]
+	HotandCold[0].tagline = "Best 24hr"
+	HotandCold[1].tagline = "Worst 24hr"
+	HotandCold[2].tagline = "Best 7d"
+	HotandCold[3].tagline = "Worst 7d"
 	marketDirection = 0
 	for i in range(0,len(stocks)):
 		marketDirection += stocks[i].week_change
